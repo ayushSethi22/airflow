4221b46f1a5423a68bd90ed8f337121fb6616f96
Sumit Maheshwari <sumeet.manit@gmail.com>
2020-10-15 15:47:18 +0530

    [EWT-499] Airflow Upgrade to 1.10.12 [CP] from 1.10.4+twtr : 199141930a626323c969a7dce245664c2a98a02e Cherry-Pick contains [CP][TWTR][EWT-377] Fix DagBag bug when a Dag has invalid schedule_interval (#61)
    CP of 5605d1063be9532a2f9861283998c39bd06e4ce8
    & https://github.com/apache/airflow/pull/11462

--- airflow/models/dagbag.py
+++ airflow/models/dagbag.py
@@ -304,9 +304,9 @@ class DagBag(BaseDagBag, LoggingMixin):
                             dag.fileloc = filepath
                     try:
                         dag.is_subdag = False
+                        if isinstance(dag._schedule_interval, six.string_types):
+                            croniter(dag._schedule_interval)
                         self.bag_dag(dag, parent_dag=dag, root_dag=dag)
-                        if isinstance(dag.normalized_schedule_interval, six.string_types):
-                            croniter(dag.normalized_schedule_interval)
                         found_dags.append(dag)
                         found_dags += dag.subdags
                     except (CroniterBadCronError,
